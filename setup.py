8fbb60b308c14554a349ebf4faec22bcf224938d
Maarten Kooyman <maarten@oyat.nl>
2025-07-04 11:19:19 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maarten-k/snakestage",
     packages=["snakestage", "pmgridtools"],
-    entry_points={"console_scripts": ["snakestage=snakestage.snakestage:main"]},
+    entry_points={"console_scripts": ["snakestage=snakestage.snakestage:main,pm_stage_files=pmgridtools.bin.pm_stage_files:main"]},
     install_requires=[
         "tqdm",
         "snakemake",
+        "requests"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
