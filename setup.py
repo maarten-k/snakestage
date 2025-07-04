481b754493ad801a422c726f80abace1639a971d
Maarten Kooyman <maarten@oyat.nl>
2025-07-04 12:07:48 +0200

    chore: update version to 0.1.4 and format entry_points in setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maarten-k/snakestage",
     packages=["snakestage", "pmgridtools"],
-    entry_points={"console_scripts": ["snakestage=snakestage.snakestage:main","pm_stage_files=pmgridtools.bin.pm_stage_files:main"]},
-    install_requires=[
-        "tqdm",
-        "snakemake",
-        "requests"
-    ],
+    entry_points={
+        "console_scripts": [
+            "snakestage=snakestage.snakestage:main",
+            "pm_stage_files=pmgridtools.bin.pm_stage_files:main",
+        ]
+    },
+    install_requires=["tqdm", "snakemake", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
