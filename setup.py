b263e2ad67cfe06a4b3793cc4b28667090f24b06
Maarten Kooyman <maarten@oyat.nl>
2025-07-04 11:40:20 +0200

    fix: correct entry_points formatting in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maarten-k/snakestage",
     packages=["snakestage", "pmgridtools"],
-    entry_points={"console_scripts": ["snakestage=snakestage.snakestage:main,pm_stage_files=pmgridtools.bin.pm_stage_files:main"]},
+    entry_points={"console_scripts": ["snakestage=snakestage.snakestage:main","pm_stage_files=pmgridtools.bin.pm_stage_files:main"]},
     install_requires=[
         "tqdm",
         "snakemake",
